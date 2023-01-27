010fa2f9c6afa5d999385b88cdd692416be15b72
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-01-27 08:05:22 -0800

    Add files via upload

---
+++ src/javascript.js
@@ -0,0 +1,10 @@
+var input_areas=document.getElementsByClassName("input_blank")
+
+function myfunction(){
+    if(input_areas[0].value.length !== 0 && input_areas[1].value.length !== 0)
+    {
+        alert("Your comment is recorded!! we will send you an E-mail for further contact.") 
+    }
+
+    
+}
